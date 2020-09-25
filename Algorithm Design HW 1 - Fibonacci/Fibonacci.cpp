bd33201a0023a2492face345f497b457d497a960
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-09-25 16:55:58 +0330

    done

--- Algorithm Design HW 1 - Fibonacci/Fibonacci.cpp
+++ Algorithm Design HW 1 - Fibonacci/Fibonacci.cpp
@@ -1,3 +1,9 @@
+/* 
+			course : Algorithm Design
+			professor : Dr. HajAhmadi
+			Project by : Parsa KamaliPour
+			Home Work 1 - Analyzing Fibonacci 
+*/
 #include <iostream>
 #include <chrono>
 #include <vector>
@@ -16,7 +22,8 @@ int FibDP(int n)
 {
 	vector<int> table;
 	table.assign(n + 1, 0);
-	table[1] = 1;
+	if (n > 0)
+		table[1] = 1;
 
 	for (int i = 2; i <= n; i++)
 		table[i] = table[i - 1] + table[i - 2];
@@ -24,8 +31,60 @@ int FibDP(int n)
 	return table[n];
 }
 
+void DoRecursive()
+{
+	cout << "enter your Nth number:" << endl;
+	int n;
+	cin >> n;
+	auto start = high_resolution_clock::now();
+	FibRecursive(n);
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+
+	cout << "Time taken by function: "
+		<< duration.count() << " microseconds" << endl;
+}
+
+void DoDP()
+{
+	cout << "enter your Nth number:" << endl;
+	int n;
+	cin >> n;
+	auto start = high_resolution_clock::now();
+	FibDP(n);
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+
+	cout << "Time taken by function: "
+		<< duration.count() << " microseconds" << endl;
+}
+
+void UI()
+{
+	cout << "Fibonacci Analyzer" << endl;
+	cout << "by Parsa KamaliPour" << endl;
+	int method;
+	while (true)
+	{
+		cout << "Select your method: (1. Recursive, 2. Dynamic Programming, 0. Exit)" << endl;
+		cin >> method;
+		if (method == 1)
+		{
+			DoRecursive();
+		}
+		else if (method == 2)
+		{
+			DoDP();
+		}
+		else if (method == 0)
+		{
+			break;
+		}
+	}
+	cin.get();
+}
+
 int main()
 {
-	cout << FibRecursive(6) << endl;
-	cout << FibDP(7) << endl;
+	UI();
 }
